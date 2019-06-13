d78ec8eb586191fa2aa8f2f8be3cd3507329a7d6
AbdurRafay <arafay@folio3.com>
2019-06-13 13:03:13 +0500

    Add files via upload

---
+++ CI_Testing/CI_Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI_Testing
+//
+//  Created by ABDUR RAFAY on 12/06/2019.
+//  Copyright © 2019 FOLIO3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
